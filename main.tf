68d17c8bb6b0f7dabd2736c49e2922bb59fcb688
imjoseangel <josea.munoz@gmail.com>
2021-12-15 08:30:59 +0100

    add main

---
+++ main.tf
@@ -0,0 +1,49 @@
+data "azurerm_subscription" "current" {}
+
+data "azurerm_resource_group" "main" {
+  name = var.resource_group
+}
+
+data "azurerm_virtual_network" "main" {
+  name                = var.virtual_network_name
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+resource "azure_subnet" "main" {
+  name                 = var.name
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = data.azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_cidr
+
+  service_endpoints = var.service_endpoints
+
+  dynamic "delegation" {
+    for_each = var.subnet_delegation
+    content {
+      name = delegation.key
+      dynamic "service_delegation" {
+        for_each = toset(delegation.value)
+        content {
+          name  = service_delegation.value.name
+          value = service_delegation.value.action
+        }
+      }
+    }
+  }
+
+  enforce_private_link_endpoint_network_policies = var.enforce_private_link
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  count = var.network_security_group_name == null ? 0 : 1
+
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = data.azurerm_network_security_group.main.id
+}
+
+resource "azurerm_subnet_route_table_association" "main" {
+  count = var.route_table_name == null ? 0 : 1
+
+  subnet_id      = azurerm_subnet.main.id
+  route_table_id = data.azurerm_route_table.main.id
+}
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "name" {
+  description = "Name of Azure Subnet."
+}
