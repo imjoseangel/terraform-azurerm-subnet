96b5e89659a2b6a76d4076c346f4a96975fa516e
imjoseangel <josea.munoz@gmail.com>
2022-11-17 20:44:28 +0100

    remove location

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@
 #-------------------------------
 locals {
   resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp[*].name, azurerm_resource_group.rg[*].name, [""]), 0)
-  location            = element(coalescelist(data.azurerm_resource_group.rgrp[*].location, azurerm_resource_group.rg[*].location, [""]), 0)
 }
 
 #---------------------------------------------------------
